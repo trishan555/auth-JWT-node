eb85d096642c6892ab79e21f20ca34cfe4a0781d
TrishLeaf <trishanp96@gmail.com>
2022-07-26 06:52:13 +0530

    signup done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,13 +1,29 @@
+const User = require('../models/user.model')
+
 const signup_get = (req, res) => {
-    res.render('signup')
+    res.send('signup')
 }
-const signup_post = (req, res) => {
-    console.log(req.body)
-    res.send('signup user')
+
+const signup_post = async (req, res) => {
+    const { email, password } = req.body
+    const newUser = {
+        email,
+        password,
+    }
+
+    try {
+        const user = await User.create(newUser)
+        res.status(201).json(user)
+    } catch (err) {
+        console.log(err)
+        res.status(400).send('User not created')
+    }
 }
+
 const login_get = (req, res) => {
     res.render('login')
 }
+
 const login_post = (req, res) => {
     res.send('user log in')
 }
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
-const mongoose = requier('mongoose')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -13,6 +15,12 @@ const userSchema = new Schema({
         required: true,
         minLength: 6,
     },
+})
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 const User = mongoose.model('user', userSchema)
